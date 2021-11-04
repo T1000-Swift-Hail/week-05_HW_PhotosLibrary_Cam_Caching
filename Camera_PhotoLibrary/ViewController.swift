2165a9538d40604121e1ad7e8174b7dca6e74c30
noyer altamimi <noyer.faisal.has.s@tuwaiq.edu.sa>
2021-11-04 16:26:23 +0300

    done

--- Camera_PhotoLibrary/ViewController.swift
+++ Camera_PhotoLibrary/ViewController.swift
@@ -20,9 +20,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Do any additional setup after loading the view.
         
         let imageToDisplay = imgStore.image(forKey: myFavImage)
-        
+     
         //Set the myImageView.image to imageToDisplay
-       
+        
+        self.myImageView.image = imageToDisplay
         
         
         
@@ -30,12 +31,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBAction func OpenPhotosLibrary(_ sender: Any) {
         // your code here
-       
+       presentPhotoPicker()
     }
     
     @IBAction func OpenCam(_ sender: Any) {
         //your code here
-       
+       presentImagePicker()
     }
     
     
@@ -70,7 +71,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let image = info[.originalImage] as! UIImage
 
         // Put that image in the imageView
-        
+        myImageView.image = image
 
         // Store the image in the ImageStore for the item's key
         imgStore.setImage(image, forKey: myFavImage)
@@ -89,7 +90,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                     
                     DispatchQueue.main.async {
                         // Put the image in the imageview
-                        
+                        self.myImageView.image = image
                     }
                 }
             }
